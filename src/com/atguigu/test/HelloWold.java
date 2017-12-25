685682c02b8d0fdf676a4f0587278ef0d5e19fbd
linghuchong <2765168717@qq.com>
2017-12-25 23:49:29 +0800

    剑法1

--- src/com/atguigu/test/HelloWold.java
+++ src/com/atguigu/test/HelloWold.java
@@ -4,5 +4,9 @@ public class HelloWold {
 
 	public static void main(String[] args) {
 		System.out.println("nihao");
+		System.out.println("nihao");
+		System.out.println("nihao");
+		System.out.println("nihao");
+		System.out.println("nihao");
 	}
 }
